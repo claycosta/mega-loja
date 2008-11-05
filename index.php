3dc0a679c08e46cdbf56dfba84e7bed5bec92e20
bighi <pspawn@gmail.com>
2008-11-05 06:46:06 -0700

    Commitando versão final da Mega Loja antes de remover menu lateral. Estável. XHTML e CSS válidos. Focado em SEO.

--- index.php
+++ index.php
@@ -1,149 +1,124 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
-  $tool_id = "5431013";
-  $url_loja = "http://xxxx/";
-  $url_site = "http://xxxx/";
-  $nome_site = "3Gamers";
+	/*
+	 * TODO coisas ainda a fazer:
+	 * - Adicionar links para ordenar por mais visitados, mais baratos, etc
+	 * - Adicionar links internos para buscas mais comuns
+	 * - Substituir barra lateral de menus por algum tipo de texto que tenha a ver com a busca
+	 *
+	 */
 
-  $user_id = 'pspawn';
-  $senha_xml = 'UFNQQVdOUFNQQVdO';
+	$tool_id = "5431013";
+	$url_loja = "http://loja.diasdebeleza.com.br/";
+	$url_site = "http://xxxx/";
+	$nome_site = "3Gamers";
 
-  $buscaPadrao = array("playstation 3", "wii", "xbox 360", "psp", "nintendo ds");
+	$user_id = 'pspawn';
+	$senha_xml = 'UFNQQVdOUFNQQVdO';
 
-  include("engine.php");
+	$buscaPadrao = array("perfume", "hidratante", "avon", "natura");
 
+	include("engine.php");
 
 /* DEFINIÇÃO DE ELEMENTOS PARA O MENU LATERAL */
 //Copie este template para vários produtos, trocando o 0 por 1, 2, 3...
 //Apenas "busca" é um parâmetro obrigatório. Todos os outros são opcionais. Se não quiser usar um deles
 //basta apagar a linha ou deixar em branco "".
 
-  $menu[0]['busca'] = "Playstation 3";  //Aqui você define o termo a ser buscado no ML
-  $menu[0]['categoria'] = "11624";      //Aqui você deve definir o ID da categoria
-  $menu[0]['preco'] = "500:3000";       //Defina preço mínimo e máximo, separado por :
-  $menu[0]['certificado'] = true;       //Se deve listar apenas vendedores certificados
-  $menu[0]['mercadopago'] = true;       //Se deve listar apenas produtos que aceitam Mercado Pago
+	$menu[0]['busca'] = "natura";	//Aqui você define o termo a ser buscado no ML
+	$menu[0]['categoria'] = "6284";			//Aqui você deve definir o ID da categoria
+	$menu[0]['preco'] = "10:200";			 //Defina preço mínimo e máximo, separado por :
+	$menu[0]['certificado'] = true;			 //Se deve listar apenas vendedores certificados
+	$menu[0]['mercadopago'] = true;			 //Se deve listar apenas produtos que aceitam Mercado Pago
 
-  $menu[1]['busca'] = "Xbox 360"; 
-  $menu[1]['categoria'] = "11108";
-  $menu[1]['preco'] = "500:3000";
-  $menu[1]['certificado'] = true;
-  $menu[1]['mercadopago'] = true;
-
-  $menu[2]['busca'] = "Wii"; 
-  $menu[2]['categoria'] = "11965";
-  $menu[2]['preco'] = "500:2000";
-  $menu[2]['certificado'] = true;
-  $menu[2]['mercadopago'] = true;
-
-  $menu[3]['busca'] = "PSP"; 
-  $menu[3]['categoria'] = "6772";
-  $menu[3]['preco'] = "400:1500";
-  $menu[3]['certificado'] = true;
-  $menu[3]['mercadopago'] = true;
-
-  $menu[4]['busca'] = "Playstation 2"; 
-  $menu[4]['categoria'] = "4384";
-  $menu[4]['preco'] = "350:1100";
-  $menu[4]['certificado'] = true;
-  $menu[4]['mercadopago'] = true;
-
+	$menu[1]['busca'] = "Xbox 360";
+	$menu[1]['categoria'] = "11108";
+	$menu[1]['preco'] = "500:3000";
+	$menu[1]['certificado'] = true;
+	$menu[1]['mercadopago'] = true;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br">
 <head>
-  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-  <meta name="keywords" content="<?= fromUrl($busca) ?>, jogos, informática, câmera, psp, playstation, wii, xbox 360, loja, videogame"/>
-  <title><?= ($busca ? 'Loja' : fromUrl($busca)) ?> - <?= $nome_site; ?></title>
-
-  <style>
-    <!--
-    body {background-image:url('http://loja.3gamers.com.br/img/fundo.png');background-repeat: repeat-y; background-position: left; background-color: #4e4741}
-    img { border:0px; }
-    a{text-decoration: none}
-    div#banner {text-align:left; margin:-10px; padding-left: 25px;background-image:url('http://loja.3gamers.com.br/img/fundbanner.jpg');background-repeat: repeat-x }
-    div#loja p {font-size:12px}
-    center {text-align: left}
-    #conteudo {margin-left:25px; margin-top:25px}
-    #conteudo form {position: relative; left:25px}
-    #menuLat{
-    width: 179px; overflow: hidden; position:absolute; left:820px; top:50px;text-align:center;
-	 padding: 0 3px;
-    background-image:url('http://loja.3gamers.com.br/img/fundoMenuLat.PNG');
-	 background-repeat: repeat-y;
-	 background-color:#ffffff;}
-    span#tituloProduto {color:black}
-    span#preco {color:red}
-    table#listagemProdutos {border-spacing: 25px 50px}
-    table#listagemProdutos td {max-width: 160px; text-align:center; border:1px black solid; overflow: hidden;}
-    -->
-  </style>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="<?= fromUrl($busca) ?>, beleza, perfumes, cabelos, natura, promoção" />
+	<meta name="language" content="pt-br" />
+	<meta name="description" content="Loja Dias de Beleza. Encontre produtos diversos para beleza feminina. Resultado da busca por: <?= fromUrl($busca) ?>." />
+	<title><?=fromUrl($busca)?>	- <?= $nome_site; ?></title>
+	<link type="text/css" rel="stylesheet" href="./loja.css" />
 </head>
 
 <body>
-  <div id='banner'>
-    <img src='http://loja.3gamers.com.br/img/logoGrafite.png'>
-  </div>
+	<div id='banner'>
+		<img src='http://loja.3gamers.com.br/img/logoGrafite.png' alt="Logotipo Loja <?=$nome_site?>: <?=fromUrl($busca)?>"/>
+	</div>
 
-  <div id='conteudo'>
-    <form action="index.php">
-      Busca: <input type="text" name="busca" value="<?= fromUrl($busca); ?>"> <input type="submit" value="Buscar">
-    </form>
-    
-    <table id="listagemProdutos">
-      <tr>
-        <?
-        $contador = 0;
-        foreach($items as $item) {
-          $contador++;
-          $title = $item->title;
-	        $link = $item->link;
-	        $image_url = $item->image_url;
-	        $price = $item->currency . $item->price;
-	        $link = str_ireplace("tool=XXX","tool=".$tool_id, $link);
-        ?>
+	<div id='conteudo'>
+		<form action="index.php">
+			<p>
+				<label for="busca">Busca:</label> <input type="text" tabindex="1" name="busca" id="busca" alt="Busca" accesskey="B" value="<?= fromUrl($busca); ?>"/>
+				<input type="submit" value="Buscar" tabindex="2" accesskey="E" />
+			</p>
+		</form>
 
-          <td>
-	          <a href="<?=$link?>">
-              <img src="<?=$image_url?>" alt="<?=$title?>"/> <br />
-	            <span id="tituloProduto"><?=$title?></span> <br />
-	            <span id="preco"><?=$price?></span>
-	          </a>
-	        </td>
-	        <? if($contador % 4 == 0) { echo("</tr><tr>"); } ?>
+		<h1>Mostrando <?= fromUrl($busca) ?></h1>
 
-        <?
-        }
-        ?>
-      </tr>
-    </table>
-  </div>
+		<table id="listagemProdutos">
+			<tr>
+				<?
+				$contador = 0;
+				foreach($items as $item) {
+					if($contador > 0 && $contador % 4 == 0) { echo("</tr><tr>"); }
+					$contador++;
+					//$title = sanitize($item->title);
+					$title = $item->title;
+					$link = $item->link;
+					$image_url = sanitize($item->image_url);
+					$price = $item->currency . $item->price;
+					$link = str_ireplace("tool=XXX","tool=".$tool_id, $link);
+					$link = sanitize($link);
+				?>
 
-<?
+					<td>
+						<a href="<?=$link?>">
+							<img src="<?=$image_url?>" alt="<?=sanitize($title)?> - <?=sanitize($busca)?>"/> <br />
+							<span class="tituloProduto"><?=$title?></span> <br />
+							<span class="preco"><?=$price?></span>
+						</a>
+					</td>
+					<?  ?>
+
+				<?
+				}
+				?>
+			</tr>
+		</table>
+		<p>Você buscou por: <strong><?= fromUrl($busca) ?></strong></p>
+	</div>
+
+<?php
 /********************************************************************************
 *********************** MENU LATERAL ********************************************
 ********************************************************************************/
 ?>
 
-  <div id='menuLat'>
-  
-    <? //TODO Permitir que a imagem do menu seja definida de forma dinâmica como variável no começo deste arquivo ?>
-    <img src='http://loja.3gamers.com.br/img/topoMenuLat.png' />
-
-    <?
-    foreach($menu as $elemento) {
+	<div id='menuLat'>
+	<div id="interno">
+	 <p>
+		<?php
+		foreach($menu as $elemento) {
 		$produto = busca_um_produto($elemento, $tool_id);
-    ?>
-      <a href="<?=$produto['link']?>">
-	      <img src="<?=$produto['image']?>" alt="<?=$produto['title']?>" /> <br />
-	      <span id="tituloProduto"><?=$produto['title']?></span> <br />
-	      <span id="preco"><?=$produto['price']?></span>
-	    </a>
-      <br /><br />
-    <?
-    }
-    ?>
-  </div>
+		?>
+			<a href="<?=$produto['link']?>">
+				<img src="<?=$produto['image']?>" alt="<?=$produto['title']?>" /> <br />
+				<span class="tituloProduto"><?=sanitize($produto['title'])?></span> <br />
+				<span class="preco"><?=$produto['price']?></span>
+			</a>
+			<br /><br />
+		<?php
+		}
+		?>
+	 </p>
+	 </div>
+	</div>
 </body>
 </html>
-
