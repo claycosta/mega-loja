2f98ed93d5afc7d7510fab7c025ed735786381ae
Leonardo Bighi <pspawn@gmail.com>
2012-01-19 13:24:44 -0200

    Versão padrão do config.php

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 	//Adicione aqui a sua ID da ferramenta no Mercado Sócios
-	$tool_id = "5432563";
+	$tool_id = "0000000";
 	
 	//Modifique a linha abaixo para indicar a página inicial da sua loja
 	//não esqueça de colocar a / (barra) no final do endereço
-	$url_loja = "http://loja.tomatecru.net/";
+	$url_loja = "http://seu.site.com/";
 	
 	//Modifique a linha abaixo para indicar o nome da sua loja.
-	$nome_loja = "Loja Tomate Cru";
+	$nome_loja = "Nome da Loja";
 	
 	//Modifique a linha abaixo para indicar seu nome de usuário no Mercado Livre
-	$user_id = 'pspawn';
+	$user_id = 'user';
 	
 	//Modifique a linha abaixo para indicar sua senha XML do mercado livre
 	//A loja funciona mesmo se você não tiver uma senha xml, mas sem ela não é possível obter
 	//mais de 10 produtos a cada busca.
-	$senha_xml = 'UFNQQVdOUFNQQVdO';
+	$senha_xml = 'coloque sua senha aqui';
 	//A senha XML pode ser obtida no fórum do Mercado Sócios, ou enviando uma mensagem diretamente
 	//para os responsáveis.
 
 	//Uma descrição da loja, que será usada para informar aos buscadores do que a sua página se trata.
 	//Use algumas palavras-chave no meio do texto.
-	$descricao_loja = "Loja Tomate Cru. De tudo um pouco, mas o preço é louco.";
+	$descricao_loja = "Mega Loja!";
 	
 	//Se o usuário acessar a página inicial da loja sem ter buscado por nada, então um dos elementos
 	//da buscaPadrao será usado aleatoriamente. O array pode conter qualquer quantidade de elementos,
